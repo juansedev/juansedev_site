f296f0d60f32510bcabb7dbb87341c7f5dc06cc1
juanllano93@gmail.com <juanllano93@gmail.com>
2020-11-06 12:43:06 -0500

    update responsive desing

--- src/components/body/body.js
+++ src/components/body/body.js
@@ -34,27 +34,55 @@ export default function Body() {
             </div>
             
             <div className="nav-links">
+              {/* <a href="#resume">About me</a> */}
               <a href="#intro">Home</a>
               <a href="#resume">Resume</a>
-              {/* <a href="">Portfolio</a>
-              <a href="">Blog</a> */}
               <a href="#contact">Contact</a>
+              
+              
+              {/* <a href="">Portfolio</a>
+              <a href="#contact">Blog</a> */}
+              
             </div>
           </nav>
           
           <main className="main-content">
             <section id="intro"className="intro">
-                <h1 className="title-first">Full-Stack Developer, Learner, and smiley person.</h1>
-                <p className="paragraph">I write code thinking in the person who will read my code, love learning, in ocasions I teaching to my friends, and if I can, I will help you to resolve your problem.</p>
-
               <div class="circle">
                 <img src={profile_photo} alt="Juan Sebastian"/>
               </div>
+
+              <div className="intro-text">
+                <h1 className="title-first">Full-Stack Developer, Learner, and smiley person.</h1>
+                <p className="paragraph">I write code thinking in the person who will read my code, love learning, in ocasions I teaching to my friends, and if I can, I will help you to resolve your problem.</p>
+                <div className="btn-contact">
+                  <button className="btn" type="button"><a href={resume} download>Download Resume</a></button>
+                  <button className="btn left-btn" ><a href="#contact" download>Contact</a></button>
+                </div>
+              </div>
+              
+
             </section>
-            <article id="resume" className="resume">
-              <h1 className="title-second">Resume</h1>
+            
+            {/* <section  className="about">
+              <h1 className="title-first center">About</h1>
+              
+            </section> */}
+
+            <section id="resume" className="resume">
+              <h1 className="title-first">Resume</h1>
               <embed className="pdf" src={resume} type="application/pdf" ></embed>
-            </article>
+            </section>
+            {/* <section  className="portfolio">
+              <h1 className="title-first center">Portfolio</h1>
+              
+            </section>
+
+            <section  className="blog">
+              <h1 className="title-first center">Blog</h1>
+              
+            </section>
+             */}
           </main>
           <footer className="footer-content">
             <article id="contact" className="icon-brand">
