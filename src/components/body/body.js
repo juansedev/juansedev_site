3f290943e60298ed7a648d32eb246866a1c2048a
juanllano93@gmail.com <juanllano93@gmail.com>
2020-11-07 00:12:43 -0500

    add new section about me and responsive desing

--- src/components/body/body.js
+++ src/components/body/body.js
@@ -1,5 +1,6 @@
 import React from "react";
 import './body.css';
+import './body_responsive_600px.css';
 
 /* pdf */
 import resume from '../asets/JuanSebastianLlanocV.pdf';
@@ -11,6 +12,9 @@ import icon_github from '../images/github.svg';
 import icon_linkedin from '../images/linkedin.svg';
 import icon_twitter from '../images/twitter.svg';
 import icon_email from '../images/email.svg';
+import developer from '../images/Developer-activity-bro.svg';
+import developer_1 from '../images/developer-1.svg';
+import launch_rocket from '../images/launching-pana.svg'; 
 
 export default function Body() {
 
@@ -22,7 +26,7 @@ export default function Body() {
             <input type="checkbox" id="nav-check"/>
 
             <div className="nav-icon">
-              <img className='logo-branding' src={baner} alt="Logo" />
+              <img className='logo-branding' src={baner} alt="Logo-juanse-developer" />
             </div>
 
             <div className="nav-btn">
@@ -34,8 +38,8 @@ export default function Body() {
             </div> 
             
             <div className="menu">
-              {/* <a href="#resume">About me</a> */}
               <a href="#intro">Home</a>
+              <a href="#about">About me</a>
               <a href="#resume">Resume</a>
               <a href="#contact">Contact</a>
               
@@ -53,15 +57,23 @@ export default function Body() {
               </div>
 
               <div className="intro-text">
-                <h1 className="title-first">Full-Stack Developer, Learner, and smiley person.</h1>
-                <p className="paragraph">I write code thinking in the person who will read my code, love learning, in ocasions I teaching to my friends, and if I can, I will help you to resolve your problem.</p>
+                <h1 className="title-first">Full-Stack Developer, learner, and smiley person.</h1>
+                <p className="paragraph">I write my code thinking about the person who will read it, I'm an enthusiastic learner, and sometimes I teach my friends. I'll do my best to help you solve your problems.</p>
                 <div className="btn-contact">
                   <button className="btn" type="button"><a href={resume} download>Download Resume</a></button>
                   <button className="btn left-btn" ><a href="#contact" download>Contact</a></button>
                 </div>
               </div>
-              
+            </section>
 
+            
+            <section id="about" className="stack">
+              <h2 className="title-first center">Hi, I’m Juan Sebastian. Nice to meet you.</h2>
+              <p className="paragraph center">
+                From the beginning of my academic and laboral life, I've been connected with the software developing world, I have programmed PIC microcontrollers, mis firsts languages were C and Dr. Scheme, I have studied about agile(SCRUM), low level, linux and web development.
+                <br />Know more about my journey in <a href="">My journey at Tech</a>
+              </p>
+              <img className='image' src={launch_rocket} alt="image-rocket-launch" />
             </section>
             
             {/* <section  className="about">
