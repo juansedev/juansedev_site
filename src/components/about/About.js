f9544c8c270550a5b3630a153b007a135367dec9
juanllano93@gmail.com <juanllano93@gmail.com>
2020-11-07 14:07:12 -0500

    update content of about component

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -21,7 +21,7 @@ export default function About() {
         </div>
 
         <div className="intro-text">
-          <h1 className="title-first">Full-Stack Developer, learner, and smiley person.</h1>
+          <h1 className="title-first">Full-Stack Developer, always learner, and smiley person.</h1>
           <p className="paragraph">I write my code thinking about the person who will read it, I'm an enthusiastic learner, and sometimes I teach my friends. I'll do my best to help you solve your problems.</p>
           <div className="btn-contact">
             <button className="btn" type="button"><a href={resume} download>Download Resume</a></button>
@@ -34,7 +34,7 @@ export default function About() {
       <section id="about" className="stack">
         <h2 className="title-first center">Hi, I’m Juan Sebastian. Nice to meet you.</h2>
         <p className="paragraph center">
-          From the beginning of my academic and laboral life, I've been connected with the software developing world, I have programmed PIC microcontrollers, mis firsts languages were C and Dr. Scheme, I have studied about agile(SCRUM), low level, linux and web development.
+          From the beginning of my academic and working life, I've been connected with the software development world, I have programmed PIC microcontrollers, mis firsts languages were C and Dr. Scheme, I have studied about agile(SCRUM), low level, linux and web development.
           <br />Know more about my journey in <Link to="/travel_in_tech">My journey at Tech</Link>.
         </p>
         <img className='image' src={launch_rocket} alt="image-rocket-launch" />
